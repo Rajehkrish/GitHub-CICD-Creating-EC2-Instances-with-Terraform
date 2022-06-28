8bb071f48b466dcf4769195b7ef985a57db2056c
suresh-subramanian2013 <98449018+suresh-subramanian2013@users.noreply.github.com>
2022-06-28 21:13:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-  }
-}
-
-provider "aws" {
-  region     = var.region
-}
-
-resource "aws_instance" "myec2" {
-   ami 			= "ami-0022f774911c1d690"
-   instance_type 	= "t2.micro"
+variable "region" {
+default = "us-east-1"
 }
