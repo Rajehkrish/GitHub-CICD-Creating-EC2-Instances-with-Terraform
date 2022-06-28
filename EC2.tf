7c83d4f94ba7626b6abebd40cb400a5384101e04
suresh-subramanian2013 <98449018+suresh-subramanian2013@users.noreply.github.com>
2022-06-28 21:13:35 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,13 +1,14 @@
-
+terraform {
+  backend "s3" {
+  }
+}
 
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 resource "aws_instance" "myec2" {
-   ami 			= "ami-0cff7528ff583bf9a"
+   ami 			= "ami-0022f774911c1d690"
    instance_type 	= "t2.micro"
 }
 
