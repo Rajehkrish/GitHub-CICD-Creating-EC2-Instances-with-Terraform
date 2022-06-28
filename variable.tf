b509df369d6cdc433eb555bebf97941bd9344ddf
suresh-subramanian2013 <98449018+suresh-subramanian2013@users.noreply.github.com>
2022-06-28 19:05:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,16 @@
 variable "region" {
 default = "us-east-1"
 }
+variable "access_key" {
+
+  type    = string
+  default = "AKIAWWWIHW7YX6J2DWCF"
+}
+variable "secret_key" {
+  type = string
+  default = "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"
+
+}
 
 
 
